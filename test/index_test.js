32b267faf8f6a68d766e52a8435cdf6b9201cebb
Rui Marinho <rui@uphold.com>
2016-08-17 17:24:04 +0100

    Trim control and whitespace from queries

--- test/index_test.js
+++ test/index_test.js
@@ -7,22 +7,27 @@
 import sql from '../src';
 
 describe('sql-tag', () => {
-  it('should support template strings with placeholders', () => {
-    const out = sql`SELECT * FROM "Foo" WHERE foo = ${'bar'} AND bar = ${1} AND baz IN (${['biz']})`;
+  it('should support template literals with placeholders', () => {
+    const out = sql`
+      SELECT * FROM biz
+      WHERE foo = ${'bar'}
+        AND bar = ${1}
+        AND baz IN (${['biz']})
+      `;
 
     out.should.eql({
-      sql: 'SELECT * FROM "Foo" WHERE foo = ? AND bar = ? AND baz IN (?)',
-      text: 'SELECT * FROM "Foo" WHERE foo = $1 AND bar = $2 AND baz IN ($3)',
+      sql: 'SELECT * FROM biz WHERE foo = ? AND bar = ? AND baz IN (?)',
+      text: 'SELECT * FROM biz WHERE foo = $1 AND bar = $2 AND baz IN ($3)',
       values: ['bar', 1, ['biz']]
     });
   });
 
-  it('should support template strings without placeholders', () => {
-    const out = sql`SELECT * FROM "Foo"`;
+  it('should support template literals without placeholders', () => {
+    const out = sql`SELECT * FROM biz`;
 
     out.should.eql({
-      sql: 'SELECT * FROM "Foo"',
-      text: 'SELECT * FROM "Foo"',
+      sql: 'SELECT * FROM biz',
+      text: 'SELECT * FROM biz',
       values: []
     });
   });
