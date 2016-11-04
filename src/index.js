7c4df7f2c1f800e57a5710299e5c546303a2840c
Rui Marinho <rui@uphold.com>
2016-11-04 01:35:53 +0000

    Remove whitespace manipulation to avoid unintended consequences

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 'use strict';
 
 /**
- * Regular expressions used for cleaning up the sql query.
- */
-
-const control = /[\t\r\n]+/gm;
-const whitespace = /\s+/gm;
-
-/**
- * Trim sql query.
- */
-
-function trim(sql) {
-  return sql.replace(control, '').replace(whitespace, ' ').trim();
-}
-
-/**
  * Export `sql-tag`.
  */
 
 export default function sqltag(query, ...values) {
   return {
-    sql: trim(query.reduce((result, part) => `${result}?${part}`)),
-    text: trim(query.reduce((result, part, index) => `${result}$${index}${part}`)),
+    sql: query.reduce((result, part) => `${result}?${part}`),
+    text: query.reduce((result, part, index) => `${result}$${index}${part}`),
     values
   };
 }
